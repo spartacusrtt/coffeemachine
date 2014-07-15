cc97be648574783e32ef2939accab23991122dc6
Aluno <me@me.com>
2014-07-15 10:02:59 -0300

    commit 1.02

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,13 +1,25 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine{
 
+	private ComponentsFactory factory;
+	private int centavos = 0;
+
 	public MyCoffeeMachine(ComponentsFactory factory) {
+		this.factory = factory;
 		factory.getDisplay().info("Insert coins and select a drink!");
-		// TODO Auto-generated constructor stub
+	}
+
+	public void insertCoin(Coin dime) {
+		centavos = (dime.getValue())%100;
+		System.out.println(centavos);
+		factory.getDisplay().info("Total: US$ +"+centavos);
+		
+		
 	}
 	
 	
