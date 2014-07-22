97047d37ec82a2b3c638e19d74f3d776ba169f4e
Aluno <me@me.com>
2014-07-22 09:10:11 -0300

    task 1.06 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -4,12 +4,14 @@ import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.Messages;
 
 public class MyCoffeeMachine implements CoffeeMachine{
 
 	private ComponentsFactory factory;
 	private int centavos = 0;
 	private int inteiro = 0;
+	private Coin dime;
 
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
@@ -18,6 +20,7 @@ public class MyCoffeeMachine implements CoffeeMachine{
 
 	public void insertCoin(Coin dime) {
 		
+		this.dime = dime;
 		if(dime == null){
 			 throw new CoffeeMachineException("null coin");
 		}
@@ -34,6 +37,9 @@ public class MyCoffeeMachine implements CoffeeMachine{
 		if(centavos == 0 && inteiro ==0){
 			 throw new CoffeeMachineException("null coin");
 		}
+		factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+		factory.getCashBox().release(dime);
+		factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
 		
 	}
 	
