078c0d7b027cd1525ac21cea971718529bfde50e
Spartacusrtt <spartacus.silva@dce.ufpb.br>
2014-08-30 13:25:58 -0300

    Task 3.05 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/GerenciadorDeMaquina.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/GerenciadorDeMaquina.java
@@ -9,7 +9,7 @@ public class GerenciadorDeMaquina {
 	private GerenciadorDeBebidas gerenteDeBebidas = new GerenciadorDeBebidas();
 	private static String modo = "";
 	private int cracha = 0;
-
+	
 	public void iniciarPedido(ComponentsFactory factory, GerenciadorDeCaixa gerenteFinanceiro, Drink drink) {
 		if (modo.equals("cracha")){
 			iniciarPedidoComCracha(factory, drink);
@@ -17,11 +17,13 @@ public class GerenciadorDeMaquina {
 			iniciarPedidoComMoedas(factory, gerenteFinanceiro, drink);
 		}
 	}
-
+		
 	public void iniciarPedidoComMoedas(ComponentsFactory factory, GerenciadorDeCaixa gerenteFinanceiro, Drink drink){
 		
 		gerenteDeBebidas.iniciarBebida(factory, drink);
-		if (!gerenteFinanceiro.conferirDinheiro(factory, gerenteDeBebidas.getValor())) {
+		
+		if (!gerenteFinanceiro.conferirDinheiro(factory,
+				gerenteDeBebidas.getValor())) {
 			return;
 		}
 
@@ -29,53 +31,60 @@ public class GerenciadorDeMaquina {
 			gerenteFinanceiro.liberarMoedas(factory, false);
 			return;
 		}
-		if (!this.gerenteDeBebidas.verificaAcucar(factory)) {
+		if (!gerenteDeBebidas.verificaAcucar(factory)) {
 			gerenteFinanceiro.liberarMoedas(factory, false);
 			return;
 		}
 
-		if (!gerenteFinanceiro.verificarTroco(factory, gerenteDeBebidas.getValor())) {
+		if (!gerenteFinanceiro.verificarTroco(factory,
+			gerenteDeBebidas.getValor())) {
 			return;
 		}
+	
 
 		gerenteDeBebidas.misturarIngredientes(factory, drink);
 		gerenteDeBebidas.release(factory);
 
 		if (gerenteFinanceiro.getTotal() >= gerenteDeBebidas.getValor()) {
-			gerenteFinanceiro.liberarTroco(factory, gerenteDeBebidas.getValor());
+					gerenteFinanceiro.liberarTroco(factory, gerenteDeBebidas.getValor());
 		}
 
 		factory.getDisplay().info(Messages.INSERT_COINS);
-		GerenciadorDeMaquina.setModo ("");
-		gerenteFinanceiro.limparMoedas();
+		setModo (" ");
+		
+		gerenteFinanceiro.limparMoedas();	
 	}
-
+	
 	public void iniciarPedidoComCracha(ComponentsFactory factory, Drink drink){
 		
 		gerenteDeBebidas.iniciarBebida(factory, drink);
-
+		
 		if (!gerenteDeBebidas.conferirIngredientes(factory, drink)) {
 			return;
 		}
 		if (!gerenteDeBebidas.verificaAcucar(factory)) {
 			return;
 		}
-		factory.getPayrollSystem().debit(gerenteDeBebidas.getValor(), cracha);
-
-		this.gerenteDeBebidas.misturarIngredientes(factory, drink);
-		this.gerenteDeBebidas.release(factory);
-
+		if(!factory.getPayrollSystem().debit(gerenteDeBebidas.getValor(), cracha)){
+			factory.getDisplay().warn(Messages.UNKNOWN_BADGE_CODE);
+			factory.getDisplay().info(Messages.INSERT_COINS);
+			setModo(" ");
+			return;
+		}
+		
+		gerenteDeBebidas.misturarIngredientes(factory, drink);
+		gerenteDeBebidas.release(factory);
+		
 		factory.getDisplay().info(Messages.INSERT_COINS);
-		GerenciadorDeMaquina.setModo (" ");
+		setModo(" ");
 	}
 
 	public void iniciarComMoedas(ComponentsFactory factory) {
-		factory.getDisplay().info(Messages.INSERT_COINS);
-		GerenciadorDeMaquina.setModo("moedas");
+			factory.getDisplay().info(Messages.INSERT_COINS);
+			setModo("moedas");
 	}
-
+	
 	public void iniciarComCracha(ComponentsFactory factory, GerenciadorDeCaixa gerenteAuxiliar, int cracha) {
-		
 		if(gerenteAuxiliar.getTotal()>0){
 			factory.getDisplay().warn(Messages.CAN_NOT_READ_BADGE);
 			return;
@@ -83,14 +92,15 @@ public class GerenciadorDeMaquina {
 		else{
 			factory.getDisplay().info(Messages.BADGE_READ);
 			this.cracha = cracha;
-			GerenciadorDeMaquina.setModo("cracha");
+			setModo("cracha");
 		}
 	}
-
+		
+	
 	public static void setModo(String novoModo) {
 		modo = novoModo;
 	}
-
+	
 	public String getModo(){
 		return modo;
 	}
